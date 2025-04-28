fcd0063a1de10ca9450db96b79712eeb1edbf0e7
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-04-28 13:37:03 +0200

    fix : redirection issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
-import { redirect } from "next/navigation"
+"use client";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
+import Cookies from "js-cookie";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
-  redirect("/sign-in")
-}
+  useEffect(() => {
+    // Check for the cookie on the client side
+    const myCookie = Cookies.get("connect.sid");
+    console.log(myCookie);
+    if (!myCookie) {
+      redirect("/sign-in");
+    } else {
+      redirect("/dashboard");
+    }
+  }, []);
 
+  return (
+    <div className="flex items-center justify-center h-64">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-orange-500" />
+        <p className="text-sm text-gray-500">Loading data...</p>
+      </div>
+    </div>
+  );
+}
