cb9e1ff83d8b6a27bcdf3eb14610045fa7130f3c
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-10-10 11:37:19 +0200

    fix : add backend URL

--- lib/redux/expenseSlice.ts
+++ lib/redux/expenseSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const expenseApi = createApi({
   reducerPath: "expenseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000/expenses",
+    baseUrl: "https://dse-backend-uv5d.onrender.com/expenses",
     credentials: "include",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('authToken');
